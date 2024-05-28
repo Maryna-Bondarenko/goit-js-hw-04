b0b32c15e3b7b5d205fc453476bada3dab899bda
Maryna-Bondarenko <158321000+Maryna-Bondarenko@users.noreply.github.com>
2024-05-28 23:53:12 +0300

    HW JS04-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,15 @@
-function slugify(title) {
-    let newTitle = title.toLowerCase();
-    while (newTitle.includes(' ')) {
-        newTitle  = newTitle.replace(' ', '-');
+function isEnoughCapacity(products, containerSize) {
+    let totalQuantity = 0;
+    const values = Object.values(products);
+    for (const value of values) {
+      totalQuantity += value;
     }
-    return newTitle;
-}
-console.log(slugify( "Arrays for beggAiners" )); // "arrays-for-begginers" 
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+    return totalQuantity <= containerSize;
+  };
+  
+  
+  console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true  
+  console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false  
+  console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true  
+  console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+  
